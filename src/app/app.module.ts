c3c597aca88332dca3cb8eea96c76c875516d4e6
herciliols <hercilioluzs@gmail.com>
2024-05-22 10:18:16 +0100

    feat: adiciona componente app-recipes-item registra no app.module - Criação do componente "app-recipes-item" - Registro do componente no módulo principal (app.module.ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { RecipesListComponent } from './recipes/recipes-list/recipes-list.compon
     HeaderComponent,
     RecipesComponent,
     RecipesListComponent,
+    RecipesItemComponent,
   ],
   imports: [
     BrowserModule
