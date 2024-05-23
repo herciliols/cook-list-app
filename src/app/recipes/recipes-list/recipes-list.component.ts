79c39622290daa98cf63ffb7def2a0d68cc00cf2
herciliols <hercilioluzs@gmail.com>
2024-05-23 09:44:37 +0100

    feat: transforma HTML do componente recipes-list de estático para dinâmico
    - Aplica a diretiva ngFor para percorrer o array de objetos instanciados
    no componente recipes-list, tornando a exibição das receitas dinâmica.

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -10,7 +10,8 @@ import { Recipe } from '../receita.model';
 
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A test Recipe 2', 'This is simply a test 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
   constructor() {}
